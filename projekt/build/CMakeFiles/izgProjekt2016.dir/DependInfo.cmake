2a97be820d10c06c2922826b52086cad68528f71
tehryn <thetehryn@gmail.com>
2017-04-10 18:06:11 +0200

    smutny zacatek

---
+++ projekt/build/CMakeFiles/izgProjekt2016.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/examples/triangleExample.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/examples/triangleExample.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/bunny.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/bunny.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/camera.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/camera.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/linearAlgebra.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/linearAlgebra.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/main.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/main.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/mouseCamera.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/mouseCamera.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/student_cpu.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/student_cpu.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/student_pipeline.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/student_pipeline.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/student_shader.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/student_shader.c.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/student/swapBuffers.c" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/student/swapBuffers.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/gpu/gpu.cpp" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/gpu/gpu.cpp.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/tests/conformanceTests.cpp" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/tests/conformanceTests.cpp.o"
+  "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/tests/performanceTest.cpp" "/mnt/sdb/FIT/FIT_druhy_rocnik_2016-2017/FIT_LS_2016/IZG/projekt/build/CMakeFiles/izgProjekt2016.dir/tests/performanceTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ projekt/student/student_shader.c
@@ -0,0 +1,83 @@
+/*!
+ * @file 
+ * @brief This file contains implemenation of phong vertex and fragment shader.
+ *
+ * @author Tomáš Milet, imilet@fit.vutbr.cz
+ */
+
+#include<math.h>
+#include<assert.h>
+
+#include"student/student_shader.h"
+#include"student/gpu.h"
+#include"student/uniforms.h"
+
+/// \addtogroup shader_side Úkoly v shaderech
+/// @{
+
+void phong_vertexShader(
+    GPUVertexShaderOutput     *const output,
+    GPUVertexShaderInput const*const input ,
+    GPU                        const gpu   ){
+  /// \todo Naimplementujte vertex shader, který transformuje vstupní vrcholy do clip-space.<br>
+  /// <b>Vstupy:</b><br>
+  /// Vstupní vrchol by měl v nultém atributu obsahovat pozici vrcholu ve world-space (vec3) a v prvním
+  /// atributu obsahovat normálu vrcholu ve world-space (vec3).<br>
+  /// <b>Výstupy:</b><br>
+  /// Výstupní vrchol by měl v nultém atributu obsahovat pozici vrcholu (vec3) ve world-space a v prvním
+  /// atributu obsahovat normálu vrcholu ve world-space (vec3).
+  /// Výstupní vrchol obsahuje pozici a normálu vrcholu proto, že chceme počítat osvětlení ve world-space ve fragment shaderu.<br>
+  /// <b>Uniformy:</b><br>
+  /// Vertex shader by měl pro transformaci využít uniformní proměnné obsahující view a projekční matici.
+  /// View matici čtěte z uniformní proměnné "viewMatrix" a projekční matici čtěte z uniformní proměnné "projectionMatrix".
+  /// Zachovejte jména uniformních proměnných a pozice vstupních a výstupních atributů.
+  /// Pokud tak neučiníte, akceptační testy selžou.<br>
+  /// <br>
+  /// Využijte vektorové a maticové funkce.
+  /// Nepředávajte si data do shaderu pomocí globálních proměnných.
+  /// Pro získání dat atributů použijte příslušné funkce vs_interpret* definované v souboru program.h.
+  /// Pro získání dat uniformních proměnných použijte příslušné funkce shader_interpretUniform* definované v souboru program.h.
+  /// Vrchol v clip-space by měl být zapsán do proměnné gl_Position ve výstupní struktuře.<br>
+  /// <b>Seznam funkcí, které jistě použijete</b>:
+  ///  - gpu_getUniformsHandle()
+  ///  - getUniformLocation()
+  ///  - shader_interpretUniformAsMat4()
+  ///  - vs_interpretInputVertexAttributeAsVec3()
+  ///  - vs_interpretOutputVertexAttributeAsVec3()
+  (void)output;
+  (void)input;
+  (void)gpu;
+}
+
+void phong_fragmentShader(
+    GPUFragmentShaderOutput     *const output,
+    GPUFragmentShaderInput const*const input ,
+    GPU                          const gpu   ){
+  /// \todo Naimplementujte fragment shader, který počítá phongův osvětlovací model s phongovým stínováním.<br>
+  /// <b>Vstup:</b><br>
+  /// Vstupní fragment by měl v nultém fragment atributu obsahovat interpolovanou pozici ve world-space a v prvním
+  /// fragment atributu obsahovat interpolovanou normálu ve world-space.<br>
+  /// <b>Výstup:</b><br> 
+  /// Barvu zapište do proměnné color ve výstupní struktuře.<br>
+  /// <b>Uniformy:</b><br>
+  /// Pozici kamery přečtěte z uniformní proměnné "cameraPosition" a pozici světla přečtěte z uniformní proměnné "lightPosition".
+  /// Zachovejte jména uniformních proměnný.
+  /// Pokud tak neučiníte, akceptační testy selžou.<br>
+  /// <br>
+  /// Dejte si pozor na velikost normálového vektoru, při lineární interpolaci v rasterizaci může dojít ke zkrácení.
+  /// Zapište barvu do proměnné color ve výstupní struktuře.
+  /// Shininess faktor nastavte na 40.f
+  /// Difuzní barvu materiálu nastavte na čistou zelenou.
+  /// Spekulární barvu materiálu nastavte na čistou bílou.
+  /// Barvu světla nastavte na bílou.
+  /// Nepoužívejte ambientní světlo.<br>
+  /// <b>Seznam funkcí, které jistě využijete</b>:
+  ///  - shader_interpretUniformAsVec3()
+  ///  - fs_interpretInputAttributeAsVec3()
+  (void)output;
+  (void)input;
+  (void)gpu;
+
+}
+
+/// @}
